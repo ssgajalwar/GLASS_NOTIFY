16b8a4d9ad1d8c883b7f6fefa330a9a9db0258f5
ssgajalwar <shreenathgajalwar@gmail.com>
2024-07-21 00:19:43 +0530

    optimized code

--- glass_notify.js
+++ glass_notify.js
@@ -1,13 +1,7 @@
-
-
-$(document).ready(function(){
-
-
-})
-function glass_notify(message,icon){
+function glass_notify(message,icon,color,bg_color){
     let ele =   `<div class="box-wrapper">
-                    <div class="box">
-                        <div class="popup"> <span>${icon}</span> <span class="poopup-text">${message}</span> </div>
+                    <div class="box" >
+                        <div class="popup" style="color:${color};"> <span>${icon}</span> <span class="poopup-text">${message}</span> </div>
                     </div>
                 </div>`
     $("body").append(ele)            
